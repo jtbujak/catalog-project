15fc53c8b5ebadae609c74acef2138b223e77c5e
John Bujak <jtbujak@msn.com>
2017-09-19 11:28:16 -0600

    Changed code to create gym database.

--- database_setup.py
+++ database_setup.py
@@ -7,24 +7,23 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Bodypart(Base):
-	__tablename__ = 'bodypart'
+class Gyms(Base):
+	__tablename__ = 'gyms'
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
 
-class Exercise(Base):
-	__tablename__ = 'exercise'
+class Amenities(Base):
+	__tablename__ = 'amenities'
 
 	name = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
 	description = Column(String(250))
-	sets = Column(String(8))
-	reps = Column(String(8))
-	bodypart_id = Column(Integer, ForeignKey('bodypart.id'))
-	bodypart = relationship(Bodypart)
+	price = Column(String(8))
+	gyms_id = Column(Integer, ForeignKey('gyms.id'))
+	gyms = relationship(Gyms)
 
 ##### insert at end of file ######
 
-engine = create_engine('sqlite:///bodybuilding.db')
+engine = create_engine('sqlite:///gyms.db')
 Base.metadata.create_all(engine)
